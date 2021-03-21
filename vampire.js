b8642b6388983a096d25fcaae807f7b5dfa7edd2
ArvinRad <radanarvin@gmail.com>
2021-03-21 06:01:45 +0000

    Tree Vampire

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.yearConverted === vampire.yearConverted;
   }
 
   /** Stretch **/
@@ -35,10 +43,22 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  // closestCommonAncestor(vampire) {
 
-  }
+  // }
 }
+
+const original = new Vampire("Original", "1980");
+const bart = new Vampire("Bart", "1985");
+const ansel = new Vampire("Ansel", "1986");
+const elgort = new Vampire("Elgort", "1990");
+const sarah = new Vampire("Sarah", "1993");
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+
 
 module.exports = Vampire;
 
